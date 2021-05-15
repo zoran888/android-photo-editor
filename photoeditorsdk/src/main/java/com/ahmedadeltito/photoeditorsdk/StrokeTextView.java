9cd10617151ad99f6679abea087b485f1e588d14
codeboy <goforward628@gmail.com>
2021-05-15 02:20:05 +0200

    fix: enable multiple line in stroke edit text

--- photoeditorsdk/src/main/java/com/ahmedadeltito/photoeditorsdk/StrokeTextView.java
+++ photoeditorsdk/src/main/java/com/ahmedadeltito/photoeditorsdk/StrokeTextView.java
@@ -1,11 +1,18 @@
 package com.ahmedadeltito.photoeditorsdk;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class StrokeTextView extends androidx.appcompat.widget.AppCompatTextView {
     private int _strokeColor;
@@ -40,17 +47,48 @@ public class StrokeTextView extends androidx.appcompat.widget.AppCompatTextView 
         return Color.parseColor("#000000");
     }
 
+    public int spToPx(float sp, Context context) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.getResources().getDisplayMetrics());
+    }
+
+    public int getDeviceWidth(Activity ctx) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ctx.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels;
+
+        return width;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         Paint p = getPaint();
+        String text = getText().toString();
+        Rect rect = new Rect();
+        p.getTextBounds("y", 0, 1, rect);
+        int sp40InPx = /*(int)p.measureText("a")*/rect.width();
+        int sp45InPx = /*spToPx(40, getContext())*/rect.height() + 20;
+        int textLength = (int)p.measureText(text);
+        int deviceWidthPx = getDeviceWidth((Activity) getContext());
+        deviceWidthPx = deviceWidthPx - (deviceWidthPx % sp40InPx);
+        int displayLimitLength = deviceWidthPx / sp40InPx;
 
-        p.setStyle(Paint.Style.FILL);
-        p.setColor(_fillColor);
-        canvas.drawText(getText().toString(), 0, getPivotY(), p);
+        for (int i = 0; i <= textLength/deviceWidthPx; i++) {
+            int endPos = (i+1)*displayLimitLength;
+            if (endPos > text.length()) {
+                endPos = text.length();
+            }
 
-        p.setStyle(Paint.Style.STROKE);
-        p.setStrokeWidth(_strokeWidth);
-        p.setColor(_strokeColor);
-        canvas.drawText(getText().toString(), 0, getPivotY(), p);
+            String lineStr = text.substring(i*displayLimitLength, endPos);
+
+            p.setStyle(Paint.Style.FILL);
+            p.setColor(_fillColor);
+            canvas.drawText(lineStr, 0, sp45InPx * i + getPivotY(), p);
+
+
+            p.setStyle(Paint.Style.STROKE);
+            p.setStrokeWidth(_strokeWidth);
+            p.setColor(_strokeColor);
+            canvas.drawText(lineStr, 0, sp45InPx * i + getPivotY(), p);
+        }
     }
 }
